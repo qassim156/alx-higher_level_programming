e9e761fd2f592feca3902ac47393d028de1fc8ec
qassim156 <iyanuoluwaqassim@gmail.com>
2024-01-10 16:52:46 -0800

    0x0a

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function write_file writes into filename and insert text
+Args:
+	filename: the file to insert text
+	text: the text to be inserted
+Return:
+	the number of characters inserted
+"""
+def write_file(filename="", text=""):
+    """ creates the file if it doeas not exist """
+    with open(filename, "w+", encoding = "utf-8") as f:
+        f.write(text)
+    """" read the file and returns the length"""
+    with open(filename, "r", encoding = "utf-8") as f:
+        line = f.read()
+    return len(line)
